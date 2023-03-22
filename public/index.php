4e51616e1b74d88182809acb6a583919a9055fec
lion-rion <lionact08@gmail.com>
2023-03-23 00:40:32 +0900

    relay処理を追加

--- public/index.php
+++ public/index.php
@@ -1,13 +1,22 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+require_once '../Http/Middleware/RouteMiddleware.php';
 
+use Http\Middleware\RouteMiddleware;
 use Nyholm\Psr7\Factory\Psr17Factory;
 use Nyholm\Psr7Server\ServerRequestCreator;
+use Relay\Relay;
 
 $psr17Factory = new Psr17Factory();
 $creator = new ServerRequestCreator($psr17Factory, $psr17Factory, $psr17Factory, $psr17Factory);
 $serverRequest = $creator->fromGlobals();
 
+//$queue = [
+//    new RouteMiddleware,
+//];
 
-dd($serverRequest);
+$queue[] = new RouteMiddleware();
+
+$relay = new Relay($queue);
+$response = $relay->handle($serverRequest);
