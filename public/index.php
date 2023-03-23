07880cd982cae5499625386575fb4218555d7475
lion-rion <lionact08@gmail.com>
2023-03-23 22:44:40 +0900

    FilterMiddlewareを追加

--- public/index.php
+++ public/index.php
@@ -2,21 +2,23 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 require_once '../Http/Middleware/RouteMiddleware.php';
+require_once '../Http/Middleware/FilterMiddleware.php';
 
-use Http\Middleware\RouteMiddleware;
+use Http\Middleware\FilterMiddleware;
 use Nyholm\Psr7\Factory\Psr17Factory;
 use Nyholm\Psr7Server\ServerRequestCreator;
 use Relay\Relay;
+use Http\Middleware\RouteMiddleware;
 
 $psr17Factory = new Psr17Factory();
 $creator = new ServerRequestCreator($psr17Factory, $psr17Factory, $psr17Factory, $psr17Factory);
 $serverRequest = $creator->fromGlobals();
+$x = new RouteMiddleware;
 
-//$queue = [
-//    new RouteMiddleware,
-//];
-
-$queue[] = new RouteMiddleware();
+$queue = [
+    new RouteMiddleware,
+    new FilterMiddleware,
+];
 
 $relay = new Relay($queue);
 $response = $relay->handle($serverRequest);
